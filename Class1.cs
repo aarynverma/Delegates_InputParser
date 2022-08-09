5c4cdaef67987b723efe7a7d16ba1efee1100371
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-09 12:31:07 +0530

    made changes

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Delegates_InputParser
+{
+    public class DelegateFunction
+    {
+        public delegate void onWord(string a);
+        public delegate void onNumber(string a);
+        public delegate void onJunk(string a);
+
+        public void word(string a)
+        {
+            if (a.All(char.IsLetter) == true)
+                Console.WriteLine("onWord");
+            else if (a.All(char.IsLetterOrDigit) == true && a.All(char.IsDigit) == false)
+                Console.WriteLine("onWord");
+        }
+
+        public void number(string a)
+        {
+            if (a.All(char.IsDigit) == true)
+                Console.WriteLine("onNumber");
+        }
+
+        public void junk(string a)
+        {
+            if (CheckSpecialChar(a) == true)
+                Console.WriteLine("onJunk");
+        }
+
+        public static bool CheckSpecialChar(string input)
+        {
+            Regex rgx = new Regex("[^A-Za-z0-9]");
+            bool hasSpecialChars = rgx.IsMatch(input);
+            return hasSpecialChars;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,59 +1,45 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using Delegates_InputParser;
+using static Delegates_InputParser.DelegateFunction;
+
 namespace Help
 {
     class ConsoleReader
     {
-        public delegate void onWord(string a);
-        public delegate void onNumber(string a);
-        public delegate void onJunk(string a);
+        private onWord word;
+        private onNumber number;
+        private onJunk junk;
 
-
-        public void word(string a)
-        {
-            if(a.All(char.IsLetter) == true)
-            Console.WriteLine("onWord");
-            else if(a.All(char.IsLetterOrDigit) == true && a.All(char.IsDigit) == false)
-            Console.WriteLine("onWord");
-        }
-
-        public void number(string a)
-        {
-            if(a.All(char.IsDigit) == true)
-            Console.WriteLine("onNumber");
-        }
-
-        public void junk(string a)
-        {
-            if (CheckSpecialChar(a) == true)
-                Console.WriteLine("onJunk");
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the value");
-            for(int i = 0; i < Console.Read(); i++)
+
+            static void run(onWord, onJunk, onNumber)
             {
+
+                Console.WriteLine("Enter the value");
                 string text = Console.ReadLine();
+/*
+                    string text = Console.ReadLine();*/
 
-                ConsoleReader Obj = new ConsoleReader();
+                for (int i = 0; i < Console.Read(); i++)
+                {
 
-                onWord obj1 = new onWord(Obj.word);
-                onNumber obj2 = new onNumber(Obj.number);
-                onJunk obj3 = new onJunk(Obj.junk);
+                    ConsoleReader Obj = new ConsoleReader();
 
-                obj1(text);
-                obj2(text);
-                obj3(text);
+                    onWord obj1 = new onWord(Obj.word);
+                    onNumber obj2 = new onNumber(Obj.number);
+                    onJunk obj3 = new onJunk(Obj.junk);
+
+                    obj1(text);
+                    obj2(text);
+                    obj3(text);
 
             }
+                }
 
         }
 
-        public static bool CheckSpecialChar(string input)
-        {
-            Regex rgx = new Regex("[^A-Za-z0-9]");
-            bool hasSpecialChars = rgx.IsMatch(input);
-            return hasSpecialChars;
-        }
+
     }
 }
